c91353321f37522a3e721ad0938c4921b6906779
rkoren <reillyhkoren@gmail.com>
2024-05-02 20:44:21 -0400

    Schedule warning

--- src/plan/js/schedule.js
+++ src/plan/js/schedule.js
@@ -1,46 +1,51 @@
-$(document).ready(function(){
-    function initializeDatepicker(parkId) {
-      $('#datepicker').datepicker({
-        format: 'yyyy-mm-dd',
-        startDate: new Date(),
-        autoclose: true,
-        beforeShowDay: function(date){
-          return isParkOpen(date, parkId);
-        }
-      });
-    }
-    // Let's re-use to instead just issue a warning if we can't confirm the park will be open
-    // Schedule data not always complete
-    function isParkOpen(date, parkId) {
-      var year = date.getFullYear();
-      var month = date.getMonth() + 1
-      // adjust to mm
-      var month = month < 10 ? '0' + month : month;
-      var day = date.getDate();
-      // adjust to dd
-      var day = day < 10 ? '0' + day : day;
-  
-      return $.ajax({
-        url: 'https://api.themeparks.wiki/v1/entity/' + parkId + '/schedule/' + year + '/' + month,
-        method: 'GET',
-        async: false,
-        success: function(response) {
-          var openDates = response.dates;
-          var formattedDate = year + '-' + month + '-' + day;
-          if (openDates.includes(formattedDate)) {
-            // Park open
-            return {enabled: true};
-          } else {
-            // Park not open
-            return {enabled: false};
-          }
-        },
-        error: function() {
-          console.log('Error occurred while fetching park schedule');
-        }
-      });
-    }
-  
-    initializeDatepicker('7502308a-de08-41a3-b997-961f8275ab3c');
+function initializeDatepicker(parkId) {
+  $('#datepicker').datepicker({
+    format: 'yyyy-mm-dd',
+    startDate: new Date(),
+    autoclose: true,
   });
+
+  $('#datepicker').on('changeDate', function(e) {
+    var selectedDate = e.date;
+    checkParkSchedule(selectedDate, parkId);
+  });
+}
+
+function checkParkSchedule(date, parkId) {
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1
+  // adjust to mm
+  var monthStr = month < 10 ? '0' + month : month;
+  var day = date.getDate();
+  // adjust to dd
+  var dayStr = day < 10 ? '0' + day : day;
+  var formattedDate = year + '-' + monthStr + '-' + dayStr;
+
+  $.ajax({
+    url: 'https://api.themeparks.wiki/v1/entity/' + parkId + '/schedule/' + year + '/' + monthStr,
+    method: 'GET',
+    success: function(response) {
+      console.log(response);
+      var dates = response.schedule;
+      const foundDate = dates.find(day => {
+        return day.date === formattedDate && day.type === "OPERATING";
+      });
+
+      if (foundDate !== undefined) {
+        return true;
+      } else {
+        alert("Cannot confirm park is open on this date!");
+        return false;
+      }
+    },
+    error: function() {
+      console.log('Error occurred while fetching park schedule');
+    }
+  });
+}
+
+$(document).ready(function(){
+  initializeDatepicker('7502308a-de08-41a3-b997-961f8275ab3c');
+});
+
   
