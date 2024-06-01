a6860f1bb1e5845c5e956e6ed97ff5f1d02e5bc0
rkoren <reillyhkoren@gmail.com>
2024-06-01 03:07:09 -0400

    Fixed modal first pass

--- src/plan/js/rideForm.js
+++ src/plan/js/rideForm.js
@@ -1,21 +1,30 @@
 var modal = document.getElementById("rideModal");
-var btn = document.getElementById("openModalBtn");
+
+var assignRidesBtn = document.getElementById("assignRidesBtn");
+var addMealsBtn = document.getElementById("addMealsBtn");
+var addShowsBtn = document.getElementById("addShowsBtn");
+
+// <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+// open the modal onclick
+assignRidesBtn.onclick = function() {
     modal.style.display = "block";
 }
 
+// close the modal on span click
 span.onclick = function() {
     modal.style.display = "none";
 }
 
+// close the modal when the user clicks anywhere outside of the modal
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
+// submit form
 document.getElementById("rideForm").onsubmit = function(event) {
     event.preventDefault();
     var formData = new FormData(this);
@@ -25,11 +34,14 @@ document.getElementById("rideForm").onsubmit = function(event) {
     });
     console.log("Form Data Submitted: ", rides);
 
+    // save the form data to local storage or send it to a server
     localStorage.setItem("rideAssignments", JSON.stringify(rides));
 
+    // close the modal
     modal.style.display = "none";
 }
 
+// load form data if available
 window.onload = function() {
     var savedRides = JSON.parse(localStorage.getItem("rideAssignments"));
     if (savedRides) {
@@ -37,4 +49,12 @@ window.onload = function() {
             document.getElementById(key).value = value;
         }
     }
+}
+
+addMealsBtn.onclick = function() {
+    alert("Add Meals functionality to be implemented");
+}
+
+addShowsBtn.onclick = function() {
+    alert("Add Shows functionality to be implemented");
 }
